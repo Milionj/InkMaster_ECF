0fdc179493f86fdf69555e2bde1c62d83bb2c5dd
Milionj <sergeweb52@mail.com>
2025-07-20 18:20:38 +0200

    docker front back

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_NAME || 'inkmaster'
 });
 
 export default db;
